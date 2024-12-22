0a00391918b95f001dd4e4654793c7915866435d
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 03:04:36 +0700

    menambahkan container untuk mengelola dependensi repository

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,5 +1,22 @@
 package com.example.ucp2.dependenciesinjection
 
-interface InterfaceContainerApp {}
+import android.content.Context
+import com.example.ucp2.data.database.KrsDatabase
+import com.example.ucp2.repository.LocalRepositoryDsn
+import com.example.ucp2.repository.LocalRepositoryMK
+import com.example.ucp2.repository.RepositoryDsn
+import com.example.ucp2.repository.RepositoryMK
 
-class ContainerApp{}
+interface InterfaceContainerApp {
+    val repositoryDsn: RepositoryDsn
+    val repositoryMK: RepositoryMK
+}
+
+class ContainerApp(private val context: Context): InterfaceContainerApp{
+    override val repositoryDsn: RepositoryDsn by lazy {
+        LocalRepositoryDsn(KrsDatabase.getDatabase(context).dosenDao())
+    }
+    override val repositoryMK: RepositoryMK by lazy {
+        LocalRepositoryMK(KrsDatabase.getDatabase(context).matakuliahDao())
+    }
+}
