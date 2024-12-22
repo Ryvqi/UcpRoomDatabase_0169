c8ffba11fd996615df61030b989ac32d4d247e21
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 03:54:09 +0700

    menambahkan rute navigasi untuk halaman dosen dan matakuliah

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -1,2 +1,25 @@
 package com.example.ucp2.ui.navigation
 
+interface AlamatNavigasi{
+    val route: String
+}
+
+object DestinasiHomeDosen : AlamatNavigasi{
+    override val route = "home dosen"
+}
+
+object DestinasiHomeMatakuliah : AlamatNavigasi{
+    override val route = "home matakuliah"
+}
+
+object DestinasiDeailMataKuliah : AlamatNavigasi{
+    override val route = "detail"
+    const val kode = "kode"
+    val routeWithArg = "$route/{$kode}"
+}
+
+object DestanasiUpdateMataKuliah : AlamatNavigasi{
+    override val route = "update"
+    const val kode = "kode"
+    val routeWithArg = "$route/{$kode}"
+}
