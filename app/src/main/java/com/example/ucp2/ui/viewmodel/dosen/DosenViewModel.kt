58ad7794e404df3eafb7545da3f3e69b7db5a86c
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 15:45:20 +0700

    menambahkan data class DsnUiState untuk mengelola UI state pada modul dosen

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.ucp2.ui.viewmodel.dosen
 
+data class DsnUiState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntityValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
