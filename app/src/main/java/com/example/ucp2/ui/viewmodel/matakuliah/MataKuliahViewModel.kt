67d18506ae7529265fb06c2dee1870ab8aa00e1c
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 16:26:33 +0700

    menambahkan data class MataKuliahEvent

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+data class MataKuliahEvent(
+    val kode: String = "",
+    val nama: String = "",
+    val sks: Int = 0,
+    val semester: Int = 0,
+    val jenis: String = "",
+    val dosenpengampu: String = "",
+)
