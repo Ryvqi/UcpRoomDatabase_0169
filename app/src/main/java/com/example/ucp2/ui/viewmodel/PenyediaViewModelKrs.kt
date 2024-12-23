6c1bcbbc006d0b4c92fbad5e201234bfcbb4a402
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 15:37:47 +0700

    menambahkan initializer untuk modul dosen dan matakuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModelKrs.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModelKrs.kt
@@ -1,2 +1,49 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KRSApp
+
+object PenyediaViewModelKrs{
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                KRSApp().containerApp.repositoryDsn
+            )
+        }
+        initializer {
+            HomeDsnViewModel(
+                KRSApp().containerApp.repositoryDsn
+            )
+        }
+
+        initializer {
+            MataKuliahViewModel(
+                KRSApp().containerApp.repositoryMK
+            )
+        }
+        initializer {
+            HomeMKViewModel(
+                KRSApp().containerApp.repositoryMK
+            )
+        }
+        initializer {
+            UpdateMKViewModel(
+                createSavedStateHandle(),
+                KRSApp().containerApp.repositoryMK
+            )
+        }
+        initializer {
+            DeleteMKViewModel(
+                createSavedStateHandle(),
+                KRSApp().containerApp.repositoryMK
+            )
+        }
+    }
+}
+
+fun CreationExtras.KRSApp(): KRSApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KRSApp)
