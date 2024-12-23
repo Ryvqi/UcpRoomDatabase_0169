49b96ed8517cb0722ddc8d0676681d1e1ef48551
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 18:33:51 +0700

    menambahkan fungsi validasi untuk update

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMKViewModel.kt
@@ -35,4 +35,18 @@ class UpdateMKViewModel(
             matakuliahEvent = matakuliahEvent,
         )
     }
+
+    private fun validateFields(): Boolean{
+        val event = updateUIState.matakuliahEvent
+        val errorState = FormErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks != 0) null else null,
+            semester = if (event.semester != 0) null else null,
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis Matakuliah tidak boleh kosong",
+            dosenpengampu = if (event.dosenpengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong",
+        )
+        updateUIState = updateUIState.copy(isEntityValid = errorState)
+        return errorState.isValid()
+    }
 }
