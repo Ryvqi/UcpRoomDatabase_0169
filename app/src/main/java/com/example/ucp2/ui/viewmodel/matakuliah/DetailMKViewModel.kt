ae4ca4f01f1f193c55867a8a003d7ec1470a0754
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 19:08:18 +0700

    menambahkan class DetailMKViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMKViewModel.kt
@@ -1,6 +1,55 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryMK
+import com.example.ucp2.ui.navigation.DestinasiDeailMataKuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailMKViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMK: RepositoryMK
+): ViewModel(){
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiDeailMataKuliah.kode])
+
+    val detailUiEvent: StateFlow<DetailUiState> = repositoryMK.getMK(_kode)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(500)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+}
 
 data class DetailUiState(
     val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
