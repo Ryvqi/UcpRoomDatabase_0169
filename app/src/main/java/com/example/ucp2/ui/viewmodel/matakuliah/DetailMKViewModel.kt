7fbf3a8695debd9bd6dc68a69058d28be10cbd1a
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 18:52:53 +0700

    menambahkan data class DetailUiState untuk mengelola ui state detail

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMKViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+data class DetailUiState(
+    val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MataKuliahEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MataKuliahEvent()
+}
