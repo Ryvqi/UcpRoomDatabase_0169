d6053bd5479aab38110ff245d7c6af0b067a9d7d
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 17:28:44 +0700

    menambahkan data class HomeUiState untuk matakuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMKViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import com.example.ucp2.data.entity.MataKuliah
+
+data class HomeUiState(
+    val listMK: List<MataKuliah> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+)
