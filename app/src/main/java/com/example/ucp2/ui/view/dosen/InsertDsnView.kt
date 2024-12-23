ef6de2e839f148194521b1569862a1839a56f685
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 20:14:21 +0700

    menambahkan fungsi InsertBodyDsn

--- app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDsnView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -15,7 +16,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
+import com.example.ucp2.ui.viewmodel.dosen.DsnUiState
 import com.example.ucp2.ui.viewmodel.dosen.FormErrorState
+
+@Composable
+fun InsertBodyDsn(
+    modifier: Modifier = Modifier,
+    onValueChange: (DosenEvent) -> Unit,
+    uiState: DsnUiState,
+    onClick: () -> Unit
+){
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormDosen(
+            dosenEvent = uiState.dosenEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntityValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+}
 
 @Composable
 fun FormDosen(
