4e44607821fa40c307dc8d68777a0c993539d09c
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-23 18:29:59 +0700

    menambahkan fungsi updatestate

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMKViewModel.kt
@@ -1,2 +1,38 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.repository.RepositoryMK
+import com.example.ucp2.ui.navigation.DestanasiUpdateMataKuliah
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMKViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMK: RepositoryMK
+): ViewModel(){
+    var updateUIState by mutableStateOf(MKUiState())
+        private set
+
+    private val _kode: String = checkNotNull(savedStateHandle[DestanasiUpdateMataKuliah.kode])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMK.getMK(_kode)
+                .filterNotNull()
+                .first()
+                .toUiStateMK()
+        }
+    }
+
+    fun updateState(matakuliahEvent: MataKuliahEvent){
+        updateUIState = updateUIState.copy(
+            matakuliahEvent = matakuliahEvent,
+        )
+    }
+}
