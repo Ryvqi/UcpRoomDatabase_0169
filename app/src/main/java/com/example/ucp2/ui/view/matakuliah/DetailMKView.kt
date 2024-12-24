50f7ab779bf6a0c6a6e48ef1fec26fead5ad8a6e
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-24 07:48:26 +0700

    menambahkan fungsi ItemDetailtMK

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMKView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMKView.kt
@@ -1,8 +1,13 @@
 package com.example.ucp2.ui.view.matakuliah
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,7 +15,42 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.MataKuliah
+
+@Composable
+fun ItemDetailMK(
+    modifier: Modifier = Modifier,
+    mataKuliah: MataKuliah
+){
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponenDetailtMK(judul = "Kode", isinya = mataKuliah.kode)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponenDetailtMK(judul = "Nama", isinya = mataKuliah.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailtMK(judul = "SKS", isinya = mataKuliah.sks.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailtMK(judul = "Semester", isinya = mataKuliah.semester.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailtMK(judul = "Jenis Matakuliah", isinya = mataKuliah.jenis)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailtMK(judul = "Dosen Pengampu", isinya = mataKuliah.dosenpengampu)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
 
 @Composable
 fun ComponenDetailtMK(
@@ -37,7 +77,6 @@ fun ComponenDetailtMK(
     }
 }
 
-)
 
 @Composable
 private fun DeleteConfirmationDialog(
