dcbb86e2ca91649b6b6037d064ca4c30c5692bc6
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-24 08:03:29 +0700

    menambahkan fungsi BodyDetailtMK

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMKView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMKView.kt
@@ -1,16 +1,23 @@
 package com.example.ucp2.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,11 +25,75 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.ui.viewmodel.matakuliah.DetailUiState
+import com.example.ucp2.ui.viewmodel.matakuliah.toMataKuliahEntity
+
+@Composable
+fun BodyDetailMK(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { }
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when{
+        detailUiState.isLoading ->
+        {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        detailUiState.isUiEventNotEmpty ->{
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMK(
+                    matakuliah = detailUiState.detailUiEvent.toMataKuliahEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequired){
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = {deleteConfirmationRequired = false},
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+        detailUiState.isUiEventNotEmpty ->{
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMK(
     modifier: Modifier = Modifier,
-    mataKuliah: MataKuliah
+    matakuliah: MataKuliah
 ){
     Card(
         modifier = modifier
@@ -35,18 +106,18 @@ fun ItemDetailMK(
         Column(
             modifier = Modifier.padding(16.dp)
         ){
-            ComponenDetailtMK(judul = "Kode", isinya = mataKuliah.kode)
+            ComponenDetailtMK(judul = "Kode", isinya = matakuliah.kode)
             Spacer(modifier = Modifier.padding(4.dp))
 
-            ComponenDetailtMK(judul = "Nama", isinya = mataKuliah.nama)
+            ComponenDetailtMK(judul = "Nama", isinya = matakuliah.nama)
             Spacer(modifier = Modifier.padding(4.dp))
-            ComponenDetailtMK(judul = "SKS", isinya = mataKuliah.sks.toString())
+            ComponenDetailtMK(judul = "SKS", isinya = matakuliah.sks.toString())
             Spacer(modifier = Modifier.padding(4.dp))
-            ComponenDetailtMK(judul = "Semester", isinya = mataKuliah.semester.toString())
+            ComponenDetailtMK(judul = "Semester", isinya = matakuliah.semester.toString())
             Spacer(modifier = Modifier.padding(4.dp))
-            ComponenDetailtMK(judul = "Jenis Matakuliah", isinya = mataKuliah.jenis)
+            ComponenDetailtMK(judul = "Jenis Matakuliah", isinya = matakuliah.jenis)
             Spacer(modifier = Modifier.padding(4.dp))
-            ComponenDetailtMK(judul = "Dosen Pengampu", isinya = mataKuliah.dosenpengampu)
+            ComponenDetailtMK(judul = "Dosen Pengampu", isinya = matakuliah.dosenpengampu)
             Spacer(modifier = Modifier.padding(4.dp))
         }
     }
